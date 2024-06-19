04573021968d72118025bbfdd3afb94e9c6f4dc2
Kevin Schuchard <schuchard.kevin@gmail.com>
2024-06-18 22:20:17 -0500

    add basicAuth to swagger

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import customerRoutes from './customer.route';
 import { swaggerUI } from '@hono/swagger-ui';
+import { basicAuth } from 'hono/basic-auth';
 
 const app = new OpenAPIHono();
 
 app.route('/customers', customerRoutes);
 
-app.get('/docs', swaggerUI({ url: '/openapi' }));
+app.get(
+  '/docs',
+  basicAuth({
+    username: 'pass',
+    password: 'word1',
+  }),
+  swaggerUI({ url: '/openapi' }),
+);
 
+app.openAPIRegistry.registerComponent('securitySchemes', 'BasicAuth', {
+  type: 'http',
+  scheme: 'basic',
+});
+
+// todo: protect the /openapi route
 app.doc31('/openapi', {
   openapi: '3.1.0',
   info: {
     version: '1.0.0',
     title: 'My API',
   },
+  security: [{ basicAuth: [] }],
 });
 
 export default app;
